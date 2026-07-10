08d62eb1ee96f28d95933825724222cd29f40382
Isaac <301175732+CryIsaac@users.noreply.github.com>
2026-07-10 15:35:02 +0400

    Gui init

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 import tts_all
 import tkinter
 from tkinter import *
+
+class GUI_TTS():
+    def __init__(self):
+        self.rot = Tk()
+        self.rot.title("TTS app")
+        width, height = 300, 500
+        self.rot.geometry(f"{width}x{height}")
+        self.rot.maxsize(400, 600), self.rot.minsize(200, 400)
+        self.rot.mainloop()
+
+if __name__ == "__main__":
+    GUI_TTS()
