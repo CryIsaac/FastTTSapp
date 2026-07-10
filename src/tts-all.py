a85e340db241656e3a4be6144e4a7ad98a9fbc21
Isaac <301175732+CryIsaac@users.noreply.github.com>
2026-07-10 15:04:38 +0400

    Base settings TTS

---
+++ src/tts-all.py
@@ -0,0 +1,56 @@
+import pyttsx3, pyaudio, torch
+from huggingface_hub import snapshot_download
+from transformers import AutoModel, AutoTokenizer, pipeline
+import sounddevice as sd
+import numpy as np
+
+def say(audio, samplerate):
+    sd.play(audio, samplerate=samplerate)
+    sd.wait()
+
+class transAutoTTS():
+    def __init__(self, model_dir, local_files_only=True):
+        self.model = AutoModel.from_pretrained(model_dir, local_files_only=local_files_only)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_dir, local_files_only=local_files_only)
+    def say(self, text:str):
+        inputs= self.tokenizer(text, return_tensore="pt")
+        with torch.no_grad:
+            outputs=self.model(**inputs)
+            audio = outputs.audio[0].cpu().numpy()
+            sr = self.model.config.samplerate
+        sd.play(audio, sr)
+        sd.wait()
+
+class transPiplineTTS():
+    def __init__(self, model_dir, local_files_only=True):
+        self.pipe = pipeline("text-to-audio", model=model_dir,local_files_only=local_files_only)
+    def say(self, text:str):
+        outputs=self.pipe(text)
+        audio= outputs["audio"]
+        sr= outputs["sampling_rate"]
+        if audio.dtype != np.float32:
+            audio = audio.astype(np.float32)
+        sd.play(audio, sr)
+        sd.wait()
+
+class pyttsx3TTS():
+    def __init__(self, voices_id:int=0): 
+        self.engine = pyttsx3.init()
+        self.voices = self.engine.getProperty("voices")
+    def settings(self, set:str, data):
+        self.engine.setProperty(str, data)
+    def say(self, text:str):
+        self.engine.say(text)
+        self.engine.runAndWait()
+        self.engine.stop()
+
+
+        
+
+        
+def download_TTSmodel(repo_id, local_dir):
+    model_path = snapshot_download(
+        repo_id=repo_id,
+        local_dir=local_dir,
+        local_dir_use_symlinks=False,
+    )
